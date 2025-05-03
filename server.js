9a751fb666fd7a41c33e8f2edad0575543fdd113
stacyhudis <stacyhudis@gmail.com>
2025-05-03 23:00:46 +0300

    Update backend

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ app.use(express.json());
 app.use(personasRoute);
 app.use('/ask', require('./routes/ask'));
 
-app.listen(3001, () => console.log('Server running on http://localhost:3001'));
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Server running on http://0.0.0.0:3001');
+  });
+  
